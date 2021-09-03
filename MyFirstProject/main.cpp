c6c2bc1644cee1643661bf619e2ff94e87ec987b
DasSuperWaffle <89536418+DasSuperWaffle@users.noreply.github.com>
2021-09-02 20:18:36 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,17 @@
+/*
+Ethan Davis
+C++ Fall 2021
+Due: September 7,2021
+Lab 1: Introduction to C++ Programming
+Download a project from GitHub, compile and run the project, upload a project to GitHub, and submit a project to Google Classroom.
+*/
+
+// Obligatory comment.
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World! Also, blue is my favorite color.";
+	return 0;
+}
